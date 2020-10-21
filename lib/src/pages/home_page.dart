edf1226e292282987b2368510905c5599f47a36c
Fecabr <fcantillob@gmail.com>
2020-10-21 17:29:53 -0600

    Se agrega dos pages navegables

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,20 +21,23 @@ Widget _lista() {
     initialData: [],
     builder: (contex, AsyncSnapshot<List<dynamic>> snaphot) {
       return ListView(
-        children: _listaItems(snaphot.data),
+        children: _listaItems(snaphot.data, contex),
       );
     },
   );
 }
 
-List<Widget> _listaItems(List<dynamic> data) {
+List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
   final List<Widget> opciones = [];
   data.forEach((opt) {
     final widgetTemp = ListTile(
       title: Text(opt['texto']),
-      leading: Icon(Icons.account_circle, color: Colors.red),
+      leading: getIcon(opt['icon']),
       trailing: Icon(Icons.keyboard_arrow_right, color: Colors.brown),
-      onTap: () {},
+      onTap: () {
+        final route = MaterialPageRoute(builder: (context) => AlertPage());
+        Navigator.push(context, route);
+      },
     );
     opciones..add(widgetTemp)..add(Divider());
   });
